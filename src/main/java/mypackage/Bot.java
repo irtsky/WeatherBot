139111591f0434b2f2df151cdf82d9a8acb4acd2
Pavel Iretskiy <127613708+irtsky@users.noreply.github.com>
2023-03-12 02:41:21 +0300

    Add api client (#1)
    * Added weather api request
    
    * Added weather api request
    
    * -1 IDE warning
    
    * -1 IDE warning
    
    * Changed catch block in Bot

--- src/main/java/mypackage/Bot.java
+++ src/main/java/mypackage/Bot.java
@@ -4,11 +4,11 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 
 public class Bot extends TelegramLongPollingBot {
 
+    //TODO: BOT_NAME is apparently unnecessary
     private final String BOT_NAME;
     private final String BOT_TOKEN;
 
@@ -31,14 +31,12 @@ public class Bot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) {
         try{
             if (update.getMessage().getText().equals("/now")) {
-
                 Message inMessage = update.getMessage();
                 String chatId = inMessage.getChatId().toString();
                 SendMessage sendMessage = new SendMessage(chatId, WeatherApiClient.getCurrentWeather());
-
                 execute(sendMessage);
             }
-        } catch (TelegramApiException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/mypackage/WeatherApiClient.java
+++ src/main/java/mypackage/WeatherApiClient.java
@@ -1,9 +1,30 @@
 package mypackage;
 
-public class WeatherApiClient {
-    private static final String URL = "";
+import org.json.JSONObject;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
-    public static String getCurrentWeather() {
-        return "Something falls from skies";
+
+//TODO: delete hardcoded API key, location
+public class WeatherApiClient {
+
+    public static String getCurrentWeather() throws Exception {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://api.weatherapi.com/v1/current.json" +
+                        "?key=***" +
+                        "&q=Saint-Petersburg"))
+                .GET()
+                .build();
+        HttpResponse<String> resp = client.send(request, HttpResponse.BodyHandlers.ofString());
+        JSONObject json = new JSONObject(resp.body());
+        return  "Currently "
+                + json.getJSONObject("current")
+                .getNumber("temp_c").toString()
+                + "C, "
+                + json.getJSONObject("current")
+                .getJSONObject("condition").getString("text");
     }
 }
