766326050679f3e812a2d05e6703a0036c1375e7
Pavel Iretskiy <iretskiy@mail.ru>
2023-03-11 23:09:46 +0300

    Now it works (kinda)

--- src/main/java/org/random/Bot.java
+++ src/main/java/org/random/Bot.java
@@ -1,21 +1,46 @@
 package org.random;
 
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
 
 public class Bot extends TelegramLongPollingBot {
+
+    private final String BOT_NAME;
+    private final String BOT_TOKEN;
+
+    public Bot(String name, String token) {
+        this.BOT_NAME = name;
+        this.BOT_TOKEN = token;
+    }
     @Override
     public String getBotUsername() {
-        return null;
+        return this.BOT_NAME;
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return this.BOT_TOKEN;
     }
 
     @Override
     public void onUpdateReceived(Update update) {
+        try{
+            if (update.getMessage().getText().equals("/start")) {
 
+                Message inMess = update.getMessage();
+                String chatId = inMess.getChatId().toString();
+                SendMessage outMess = new SendMessage();
+
+                outMess.setChatId(chatId);
+                outMess.setText("hi");
+                execute(outMess);
+            }
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/org/random/Main.java
+++ src/main/java/org/random/Main.java
@@ -1,7 +1,29 @@
 package org.random;
 
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Work in progress");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Enter bot name");
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in));
+        String name = reader.readLine();
+
+        System.out.println("Enter token");
+        String token = reader.readLine();
+
+        try {
+            TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+            botsApi.registerBot(new Bot(name, token));
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
 }
